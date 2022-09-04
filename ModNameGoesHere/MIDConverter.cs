cb930f9eb16c90cc8408823f18bd99bfc7865dcd
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-09-04 17:25:23 -0500

    Big bang

---
+++ ModNameGoesHere/MIDConverter.cs
@@ -0,0 +1,82 @@
+﻿using BaseX;
+using System;
+using System.IO;
+using System.Diagnostics;
+using FrooxEngine;
+using CodeX;
+using System.Threading.Tasks;
+
+namespace MIDImporter
+{
+    public class MIDConverter
+    {
+        private static readonly string executablePath = Path.Combine("nml_mods", "midi_importer");
+        private static readonly string windowsExecutable = "fluidsynth.exe";
+        private static readonly string windowsArgs = "-ni {0} {1} --fast-render {2}.wav";
+        private static readonly string macOSXCommand;
+        private static readonly string unixCommand;
+
+        public async static Task Convert(string band, string inputFile, string outputPath, string outputName)
+        {
+            var platform = Environment.OSVersion.Platform;
+            switch (platform)
+            {
+                // According to https://docs.microsoft.com/en-us/dotnet/api/system.platformid?view=netframework-4.7a.2
+                // some platform enums are no longer in use. I've left them here:
+                // - case PlatformID.Win32S
+                // - case PlatformID.Win32Windows
+                // - case PlatformID.WinCE
+                // - case PlatformID.Xbox:
+
+                case PlatformID.Win32NT:
+                    await PerformWindowsUnpack(band, inputFile, outputPath, outputName);
+                    break;
+                case PlatformID.Unix:
+                    throw new PlatformNotSupportedException("Unix support has not been added yet! Scream at dfg if you see this!");
+                case PlatformID.MacOSX:
+                    throw new PlatformNotSupportedException("MacOS support has not been added yet! Scream at dfg if you see this!");
+                default:
+                    throw new PlatformNotSupportedException("You've managed to run this on a platform .NET doesn't recognize, how are you playing Neos?");
+            }
+        }
+
+        private async static Task PerformWindowsUnpack(string band, string inputFile, string outputPath, string outputName)
+        {
+            var windowsExecutablePath = Path.GetFullPath(Path.Combine(executablePath, windowsExecutable));
+
+            if (!File.Exists(windowsExecutablePath))
+                throw new FileNotFoundException("Could not find fluidsynth. Is it present under nml_mods/midi_converter/fluidsynth.exe?");
+
+            string formattedWindowsArgs = string.Format(
+                windowsArgs, 
+                band, 
+                inputFile, 
+                Path.Combine(outputPath, outputName));
+
+            UniLog.Log($"Starting MIDI conversion with the following command: {windowsExecutablePath} {formattedWindowsArgs}");
+
+            var process = new Process();
+            process.StartInfo.FileName = windowsExecutablePath;
+            process.StartInfo.Arguments = formattedWindowsArgs;
+            process.StartInfo.UseShellExecute = true;
+            process.StartInfo.CreateNoWindow = false;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            process.Start();
+            process.WaitForExit(); // Blocks?
+
+            UniLog.Log("MIDI extraction complete!");
+
+            await Task.CompletedTask;
+        }
+
+        private static void PerformMacOSXConvert(string inputFile, string outputPath, int? optionalThreads)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void PerformUniXConvert(string inputFile, string outputPath, int? optionalThreads)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ModNameGoesHere/MIDImporter.cs
@@ -0,0 +1,91 @@
+﻿using BaseX;
+using CodeX;
+using FrooxEngine;
+using HarmonyLib;
+using NeosModLoader;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MIDImporter
+{
+    public class MIDImporter : NeosMod
+    {
+        public override string Name => "MIDImporter";
+        public override string Author => "dfgHiatus";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/dfgHiatus/MIDImporter";
+
+        private static readonly string convertedMidPath = Path.Combine(Engine.Current.CachePath, "Cache", "ConvertedMIDIs");
+        private static readonly string bandPath = Path.Combine("nml_mods", "midi_importer", "bands");
+        private static readonly string defaultBandName = "GeneralUser_GS_v1.471.sf2";
+        private static ModConfiguration config;
+
+        [AutoRegisterConfigKey]
+        public static ModConfigurationKey<string> bandName = new ModConfigurationKey<string>("band", "MIDI Converter file path", () => defaultBandName);
+
+        public override void OnEngineInit()
+        {
+            new Harmony("net.dfgHiatus.MIDImporter").PatchAll();
+            Directory.CreateDirectory(convertedMidPath);
+            config = GetConfiguration();
+            Engine.Current.RunPostInit(() => AssetPatch());
+        }
+
+        public static void AssetPatch()
+        {
+            var aExt = Traverse.Create(typeof(AssetHelper)).Field<Dictionary<AssetClass, List<string>>>("associatedExtensions");
+            aExt.Value[AssetClass.Special].Add("mid");
+        }
+
+        [HarmonyPatch(typeof(UniversalImporter), "ImportTask", typeof(AssetClass), typeof(IEnumerable<string>), 
+            typeof(World), typeof(float3), typeof(floatQ), typeof(float3), typeof(bool))]
+        public class UniversalImporterPatch
+        {
+            static bool Prefix(IEnumerable<string> files, ref Task __result, World world)
+            {
+                var query = files.Where(x => x.ToLower().EndsWith(".mid"));
+                if (query.Count() > 0)
+                {
+                    __result = ProcessMIDImport(query, world);
+                }
+                return true;
+            }
+        }
+
+		private static async Task ProcessMIDImport(IEnumerable<string> files, World world)
+		{
+			await default(ToBackground);
+
+            if (!File.Exists(Path.Combine(bandPath, config.GetValue(bandName))))
+            {
+                if (!File.Exists(Path.Combine(bandPath, defaultBandName)))
+                {
+                    Error($"The default MID converter {defaultBandName} was not found. Are you missing this file under nml_mods/midi_converter/bands?");
+                    return;
+                }
+                config.Set(bandName, defaultBandName);
+            }
+
+            var fullBandPath = Path.Combine(bandPath, config.GetValue(bandName));
+            LocalDB localDB = world.Engine.LocalDB;
+            List<string> list = new List<string>();
+            foreach (var inputMid in files)
+            {
+                var convertedMidName = Path.GetFileNameWithoutExtension(inputMid);
+                await MIDConverter.Convert(fullBandPath, inputMid, convertedMidPath, convertedMidName).ConfigureAwait(false);
+                list.Add(Path.GetFullPath(Path.Combine(convertedMidPath, convertedMidName)));
+            }
+
+            foreach (var convertedFiles in list)
+            {
+                Msg(convertedFiles + ".wav");
+                await localDB.ImportLocalAssetAsync(
+                    Path.Combine(convertedFiles + ".wav"),
+                    LocalDB.ImportLocation.Copy).
+                    ConfigureAwait(continueOnCapturedContext: false);
+            }
+        }
+	}
+}
